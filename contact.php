c4601670f4840e18c2f910a5bcb70311dde84670
Jonathan Delves <jonathandelves@hotmail.com>
2025-07-30 14:54:42 +0100

    Contact section

---
+++ contact.php
@@ -0,0 +1,7 @@
+<?php /* Template Name: Contact */ ?>
+<?php get_header(); ?>
+<div class="contact-section">
+    <span class="circles"></span>
+    <?php include "includes/onpage-contact-form.php"; ?>
+</div>
+<?php get_footer(); ?>
